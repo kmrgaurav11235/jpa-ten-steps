d8ea9d0c6e5f3a8126a878d8a159d5c219d931ec
Kumar Gaurav <kgaurav@adobe.com>
2019-12-13 12:21:42 +0530

    JPA Initialized

---
+++ src/main/java/com/gaurav/jpa/JpaTenStepsApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTenStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTenStepsApplication.class, args);
+	}
+
+}
